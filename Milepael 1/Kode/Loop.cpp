3c50d8cc60f0127979967878a0a64b2b9e6ae941
lordlop12 <lordlop12@yahoo.dk>
2024-09-27 11:23:08 +0200

    Test

---
+++ Milepael 1/Kode/Loop.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // For-løkke, der starter ved 1 og slutter ved 100
+    for (int i = 1; i <= 100; i++) {
+        std::cout << i << "\n";
+    }
+
+    return 0;
+}
